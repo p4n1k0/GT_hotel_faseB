4de5cf8398ce752d3df83c413f7b492c204f94ee
p4n1k0 <gabre.tarick@gmail.com>
2025-02-22 19:42:02 -0300

    models

--- src/TrybeHotel/Models/Room.cs
+++ src/TrybeHotel/Models/Room.cs
@@ -11,4 +11,5 @@ public class Room {
     public string? Image { get; set; }
     public int HotelId { get; set; }
     public Hotel? Hotel { get; set; }
+    public ICollection<Booking>? Bookings { get; set; }
 }
--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,13 +4,23 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) {
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+
+    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
+    {
         Seeder.SeedUserAdmin(this);
     }
     public TrybeHotelContext() { }
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 
 }
